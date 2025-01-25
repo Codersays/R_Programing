53b79fd0209a20532c86826c15821aec914f86b2
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-01-25 14:34:31 +0530

    Create Practical Slips 16.R

---
+++ Practical Slips 16.R
@@ -0,0 +1,16 @@
+16. Write an R Program to calculate Decimal into binary of a given number.
+
+convert_to_binary <- function(n) {
+
+if(n > 1) {
+
+convert_to_binary(as.integer(n/2))
+
+}
+
+cat(n %% 2)
+
+}
+
+
+
