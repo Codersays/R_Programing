3f228815a0cee014953d2e0f28482dc4014da457
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-01-25 15:05:33 +0530

    Create Practical Slips 22.R

---
+++ Practical Slips 22.R
@@ -0,0 +1,38 @@
+22. Write an R program to print the numbers from 1 to 100 and print "SY" for multiples of 3, print "BBA" for multiples of 5, and print "SYBBA" for multiples of both.
+
+
+
+for (n in 1:100)
+
+{
+
+if (n %% 3 == 0 & n %% 5 == 0)
+
+{
+
+print("SY")
+
+}
+
+else if (n %% 3 == 0)
+
+{
+
+print("BBA")
+
+}
+
+else if (n %% 5 == 0)
+
+{
+
+print("SYBBA")
+
+}
+
+else print(n)
+
+}
+
+
+
