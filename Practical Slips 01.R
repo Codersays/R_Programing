f96f608223baaf647f65493538120112cda9b6be
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-01-25 10:04:35 +0530

    Create Practical Slips 01.R

---
+++ Practical Slips 01.R
@@ -0,0 +1,13 @@
+1.Write an R program to find the maximum and the minimum value of a given vector.
+
+nums = c(10, 20, 30, 40, 50, 60)
+
+print('Original vector:')
+
+print(nums)
+
+print(paste("Maximum value of the said vector:",max(nums)))
+
+print(paste("Minimum value of the said vector:",min(nums)))
+
+
