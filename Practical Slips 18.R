69adbfd6dcb10b151e1da3fb1abe2b7ea38c691f
Ankit Siddharth Thool <thoolankit10@gmail.com>
2025-01-25 14:41:46 +0530

    Create Practical Slips 18.R

---
+++ Practical Slips 18.R
@@ -0,0 +1,12 @@
+18. Write an R program to draw an empty plot and an empty plot specify the axes limits of the graphic.
+
+#print("Empty plot:")
+
+plot.new()
+
+#print("Empty plot specify the axes limits of the graphic:")
+
+plot(1, type="n", xlab="", ylab="", xlim=c(0, 20), ylim=c(0, 20))
+
+
+
